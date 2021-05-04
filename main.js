6aad0b9112547c8f0257212544b93037083c67f9
JumaGypsum <justuskeya1@gmail.com>
2021-05-05 00:29:39 +0300

    Update 1.0

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-//VARIABLES
-let filterInput = document.getElementById('table-input');
+// //VARIABLES
+// let input = document.getElementById('table-input');
+// input.addEventListener('keyup', filterProducts);
 
-//EVENTLISTENERS
-filterInput.addEventListener('keyup', filterProducts);
+// function filterProducts(){
+//     let input = document.getElementById('table-input');
+//     let filter = input.value.toUpperCase();
+//     let table = document.getElementById('table');
+//     let tr = table.getElementsByTagName('tr');
 
+//     for(let i = 0; i < tr.length; i++){
+//         let td = tr[i].getElementsByTagName('td')[0];
+//         let td2 = tr[i].getElementsByTagName('td')[1];
+//         if(td){
+//             let textValue = td.innerHTML ||td.textContent;
+//             if(textValue.toUpperCase().indexOf(filter)> -1){
+//                 tr[i].style.display = '';
+//             }else{
+//                 tr[i].style.display = 'none';
+//             }
+//         }
+//     }
+// }
+
+// let filterInput = document.getElementById('table-input');
+// filterInput.addEventListener('keyup', filterProducts);
+// const login = document.getElementById('login').addEventListener('click', function(){
+//     window.open('login.html','popup','width=801,height=800');
+//     return false;
+// });
+
+//HAMBUGER MENU
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+})     
 
 
 //FUNCTIONS
@@ -39,4 +70,3 @@ function filterProducts(){
         }
     }
 }
-
